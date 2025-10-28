95890172a3a3bd210d9b6cef23af912f0a206dcd
Andrii Kravchenko <maks123pop123lol@gmail.com>
2025-10-28 21:36:24 +0200

    Доданий сервер Node.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    const filePath = path.join(__dirname, "a.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Ошибка сервера");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен: http://localhost:${PORT}`);
+});
